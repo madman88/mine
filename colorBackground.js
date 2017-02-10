88a76304e02f8e5da4c61dd75196456c9efc64d3
madman88 <madhusudan.prahlad@gmail.com>
2017-02-10 09:30:07 -0800

    revealing module pattern

--- colorBackground.js
+++ colorBackground.js
@@ -1,24 +1,28 @@
 
-var colorBackground = {
-  usedColors: {},
+var colorBackground = (function(){
+  var usedColors = {};
 
-  isUsed: function(randomColor) {
-    return this.usedColors[randomColor] === true;
-  },
-
-  getRandomColor: function(){
-    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    if (this.isUsed(randomColor)){
-      this.getRandomColor();
-    }
-    this.usedColors[randomColor] = true;
-    return randomColor;
-  },
-
-  colorBackground: function() {
-    document.getElementById("body").style["background-color"] = this.getRandomColor();
+  var isUsed = function(randomColor) {
+    return usedColors[randomColor] === true;
   }
-};
+
+  var getRandomColor =  function(){
+    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    if (isUsed(randomColor)){
+      getRandomColor();
+    }
+    usedColors[randomColor] = true;
+    return randomColor;
+  }
+
+  var color = function() {
+    document.getElementById("body").style["background-color"] = getRandomColor();
+  }
+
+  return {
+    colorBackground: color
+   };
+})();
 
 
 var colorBackground_id = setInterval(function(){ colorBackground.colorBackground() }, 1000);
